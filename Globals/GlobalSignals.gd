6694155b5b6a105a4adec8dcf13dbe9107d7adcb
Cassio-4 <cassiobertasinascimento@gmail.com>
2024-07-31 04:22:18 -0400

    dialogs

--- Globals/GlobalSignals.gd
+++ Globals/GlobalSignals.gd
@@ -3,4 +3,10 @@ extends Node
 signal projectile_fired(projectile_instance, team, location, direction)
 signal player_lamp_changed(is_lamp_active)
 signal player_spawned(player)
+<<<<<<< Updated upstream
 signal enemy_died(a_position)
+=======
+
+func change_scene(scene_path: String):
+	get_tree().change_scene_to_file(scene_path)
+>>>>>>> Stashed changes
---
+++ shop_scenes/runTimeline.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Dialogic.start("timeline")
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
